479605933740a91f2bf94adb0befaee189be07c4
TomBibish <tombibish8@gmail.com>
2022-03-15 10:31:46 +0200

    First TRY

--- src/TeamDetails.js
+++ src/TeamDetails.js
@@ -7,7 +7,8 @@ export  class TeamDetails extends React.Component{
         this.state= {
             team: {},
             stadium:{},
-            league:{}
+            league:{},
+            coach: {}
         }
     }
     componentDidMount() {
@@ -18,6 +19,11 @@ export  class TeamDetails extends React.Component{
                 stadium: res.data.stadium,
                 league: res.data.league
             }))
+        axios
+            .get('http://127.0.0.1:8000/api/v1/team_coach/' + this.props.team.replace(" ", "%20"))
+            .then(res => this.setState({
+                coach: res.data.coach}))
+        console.log(this.state.coach)
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
@@ -27,27 +33,40 @@ export  class TeamDetails extends React.Component{
 
     render() {
         return(
-                <div className={'center'}>
-                    <Card style={{ width: '18rem' }}>
-                      <Card.Img variant="top" src={this.state.team.picture_url} />
-                      <Card.Body>
-                        <Card.Title>{this.state.team.name}</Card.Title>
-                        <Card.Text>
-                          {this.state.team.name} which play in the {this.state.league.name}
-                        </Card.Text>
-                      </Card.Body>
-                      <ListGroup className="list-group-flush">
-                        <ListGroupItem>Points this season: {this.state.team.points}</ListGroupItem>
-                        <ListGroupItem>Goals - {this.state.team.goals_for}:{this.state.team.goals_against}</ListGroupItem>
-                        <ListGroupItem>
-                            Play home games in {this.state.stadium.name} ({this.state.stadium.capacity})
-                        </ListGroupItem>
-                      </ListGroup>
-                      <Card.Body>
-                        <Card.Link href="#"><img className={'home-icon'} src={this.state.league.picture_url}/> </Card.Link>
-                        <Card.Link href="#">{}</Card.Link>
-                      </Card.Body>
-                    </Card>
+                <div className={'float-container '}>
+                    <div className={'float-child'}>
+                        <Card style={{ width: '18rem' }}>
+                          <Card.Img variant="top" src={this.state.team.picture_url} />
+                          <Card.Body>
+                            <Card.Title>{this.state.team.name}</Card.Title>
+                            <Card.Text>
+                              {this.state.team.name} which play in the {this.state.league.name}
+                            </Card.Text>
+                          </Card.Body>
+                          <ListGroup className="list-group-flush">
+                            <ListGroupItem>Points this season: {this.state.team.points}</ListGroupItem>
+                            <ListGroupItem>Goals - {this.state.team.goals_for}:{this.state.team.goals_against}</ListGroupItem>
+                            <ListGroupItem>
+                                Play home games in {this.state.stadium.name} ({this.state.stadium.capacity})
+                            </ListGroupItem>
+                          </ListGroup>
+                          <Card.Body>
+                            <Card.Link href="#"><img className={'home-icon'} src={this.state.league.picture_url}/> </Card.Link>
+                            <Card.Link href="#">{}</Card.Link>
+                          </Card.Body>
+                        </Card>
+                    </div>
+                    <div className={'float-child'}>
+                        <Card style={{ width: '18rem' }}>
+                          <Card.Img variant="top" src={this.state.coach.picture_url} />
+                          <Card.Body>
+                            <Card.Title>{this.state.coach.first_name} {this.state.coach.last_name}</Card.Title>
+                            <Card.Text>
+                              Active coach of {this.state.team.name}
+                            </Card.Text>
+                          </Card.Body>
+                        </Card>
+                    </div>
                 </div>
         )
     }
