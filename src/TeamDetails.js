ed164f6e2d128bddabba50920c652283dfad170c
TomBibish <tombibish8@gmail.com>
2022-03-21 09:09:32 +0200

    First TRY

--- src/TeamDetails.js
+++ src/TeamDetails.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from "axios";
-import {Card, Carousel, ListGroup, ListGroupItem, Table} from "react-bootstrap";
+import {Card, ListGroup, ListGroupItem, Table} from "react-bootstrap";
 export  class TeamDetails extends React.Component{
     constructor(props) {
         super(props);
