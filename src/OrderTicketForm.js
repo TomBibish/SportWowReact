a7056700a4c67d76b9a082bc0028eef75bf391ba
TomBibish <tombibish8@gmail.com>
2022-03-27 15:27:13 +0300

    First TRY

---
+++ src/OrderTicketForm.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form'
+import { Button } from 'react-bootstrap';
+import axios from 'axios';
+
+export class OrderTicketForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state ={
+            amount:0,
+            user:{}
+        }
+
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    componentDidMount() {
+        const token = window.localStorage.getItem('token')
+        axios
+            .get('http://127.0.0.1:8000/api/v1/users/current', {headers: {Authorization: 'Token ' + token}})
+            .then(res =>this.setState({user:res.data}))
+    }
+
+    handleSubmit() {
+        axios.post('http://127.0.0.1:8000/api/v1/ordered_tickets', {ticket:this.props.ticket_id,
+            user:this.state.user.id,
+            amount:this.state.amount
+        })
+        .then(() => {
+            window.alert(this.state.amount + ' tickets ordered')})
+        .catch(error => window.alert(error))
+        this.props.onHide()
+    }
+
+    render () {
+        return(
+        <Modal show={this.props.show} onHide={this.props.onHide}>
+        <Modal.Header closeButton>
+        <Modal.Title>Purchase Ticket</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form>
+            <Form.Group className="mb-3">
+                <Form.Label>Amount</Form.Label>
+                <Form.Text>
+                    <Form.Control
+                        type="text" placeholder="amount"
+                        value={this.state.amount}
+                        aria-valuemax={10}
+                        onChange={(event) => this.setState({amount: event.target.value})}/>
+                </Form.Text>
+            </Form.Group>
+        </Form>
+        </Modal.Body>
+        <Modal.Footer>
+        <Button variant="secondary" onClick={this.props.onHide}>
+            Cancel
+        </Button>
+        <Button variant="primary"
+            onClick={this.handleSubmit}>
+            Order
+        </Button>
+        </Modal.Footer>
+        </Modal>
+        )
+
+    }
+}
--- src/OrderTickets.js
+++ src/OrderTickets.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form'
 import {Button, Card, CardGroup, Container} from 'react-bootstrap';
 import axios from 'axios';
+import {OrderTicketForm} from "./OrderTicketForm";
 
 export class OrderTickets extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            tickets:[]
+            tickets:[],
+            ticket_picked:1,
+            showOrderForm:false
         }
-
+        this.handleSubmit = this.handleSubmit.bind(this)
         this.renderTickets = this.renderTickets.bind(this)
     }
     componentDidMount() {
@@ -20,9 +21,12 @@ export class OrderTickets extends React.Component {
             .get('http://127.0.0.1:8000/api/v1/tickets', {headers: {Authorization: 'Token ' + token}})
             .then(res =>this.setState({tickets:res.data}))
     }
+    handleSubmit(ticket_id){
+        this.setState({ticket_picked:ticket_id, showOrderForm:true})
+    }
     renderTickets(ticket){
         return(
-                  <Card border={'primary'} style={{margin:"10px"}}>
+                  <Card key={ticket.id} border={'primary'} style={{margin:"10px"}}>
                     <Card.Img style={{height:"90%"}} variant="top" src={ticket.match.home_team.stadium.picture_url}/>
                     <Card.Body style={{textAlign:"center"}}>
                       <Card.Title><img alt={""} className={"table-icon-league"} src={ticket.match.home_team.picture_url}/>
@@ -37,7 +41,7 @@ export class OrderTickets extends React.Component {
                     </Card.Body>
                     <Card.Footer>
                         <div className={"center-button"}>
-                            <Button>Order for {ticket.price}$</Button>
+                            <Button onClick={()=>this.handleSubmit(ticket.id)}>Order for {ticket.price}$</Button>
                         </div>
                     </Card.Footer>
                   </Card>
@@ -53,7 +57,10 @@ export class OrderTickets extends React.Component {
                 <CardGroup style={{width:"100%"}}>
                     {ticketsObject}
                 </CardGroup>
+                <OrderTicketForm show={this.state.showOrderForm} onHide={() => this.setState({showOrderForm: false})}
+                              ticket_id={this.state.ticket_picked}/>
             </Container>
+
         )
     }
 }
